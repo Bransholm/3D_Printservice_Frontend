9c56485a510b7f9dab409ec2c987d84d7a717d04
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-12-08 18:12:40 +0100

    admin changes

--- admin-main.js
+++ admin-main.js
@@ -8,37 +8,32 @@ window.addEventListener("load", startAdmin);
 
 // import { createNewMaterial } from "./create-new-stock-item.js";
 import { createNewCatalogueItem } from "../create-update-forms/create-new-catelogue-item.js";
-import { getCatalogueData } from "./fetch-data.js";
+import { getCatalogueData, getStockData } from "./fetch-data.js";
 import { createCatalogClasses } from "./classes-test-doc.js";
-
-
+import { stockMaterial } from "./view-render-classes/stock-class.js";
 
 function startAdmin() {
   console.log("Admin site is working");
   activateEventListeners();
-  readAllCatalogueItems();
+  getAllData();
 }
 
-async function readAllCatalogueItems() {
-  const catelogueData = await getCatalogueData();
-  // createCatalogClasses(catelogueData);
+async function getAllData() {
+  const Materials = await getStockData();
+  console.log("material list: ", Materials);
+  const CatalougeItems = await getCatalogueData();
+  createCatalogClasses(CatalougeItems);
+}
 
-  }
+// async function readAllCatalogueItems() {
+//   const catelogueData = await getCatalogueData();
+//   // createCatalogClasses(catelogueData);
+//   }
 
 function activateEventListeners() {
   document
     .querySelector("#addToCatalogueForm")
     .addEventListener("submit", createNewCatalogueItem);
-  document.querySelector("#test_form").addEventListener("submit", test);
-}
-
-function test(event) {
-  console.log(event);
-  event.preventDefault();
-
-  const testValue = event.target.someText.value;
-
-  console.log(testValue);
 }
 
 export { startAdmin as launchAdminFunctions };
