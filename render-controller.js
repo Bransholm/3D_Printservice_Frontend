c019d26712d34cfae4f003a24af143b0f8a4f12b
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-12-10 13:58:48 +0100

    count prints test

--- render-controller.js
+++ render-controller.js
@@ -1,5 +1,7 @@
 import { viewButtonClicked } from "./classes-test-doc.js";
 
+let runte = 0;
+
 export function callRenderMethod(listOfInstances, htmlId) {
   console.log("No3. CallRenderMethod");
   document.querySelector(`#${htmlId}`).innerHTML = "";
@@ -7,15 +9,16 @@ export function callRenderMethod(listOfInstances, htmlId) {
   for (const instance of listOfInstances) {
     const classHTML = instance.render();
 
+    runte += 1;
+    console.log("Elementer vist: ", runte);
+
     document
       .querySelector(`#${htmlId}`)
       .insertAdjacentHTML("beforeend", classHTML);
-    
-      eventListenerAdder(htmlId, instance);
+
+    eventListenerAdder(htmlId, instance);
   }
 
-
-  
   function eventListenerAdder(htmlId, classInstance) {
     // what eventlisteners to add for a given instance needs to go here...
 
@@ -24,5 +27,3 @@ export function callRenderMethod(listOfInstances, htmlId) {
       .addEventListener("click", () => viewButtonClicked(classInstance));
   }
 }
-
-
