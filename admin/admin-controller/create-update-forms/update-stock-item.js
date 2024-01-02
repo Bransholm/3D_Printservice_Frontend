7dab5f7c42db8fdb427aa68af47a587f1ca356ad
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-02 13:06:10 +0100

    active name correction

--- admin/admin-controller/create-update-forms/update-stock-item.js
+++ admin/admin-controller/create-update-forms/update-stock-item.js
@@ -3,12 +3,17 @@ function extractStockDataForUpdate(stockMaterial) {
   const updateForm = document.querySelector("#updateMaterialForm");
   // const x = event.target
 
+
+
+  // ---------------------------------------------- DEV NOTE: We changed minAmountReached = "1" to active = 1 ;
+  // Change it i  HTML and all JS... otherwise it will never work...
+
   // Doublecheck if attirbute needs to be with capital sarting letter...
   updateForm.name.value = stockMaterial.name;
   updateForm.material.value = stockMaterial.material;
   updateForm.colour.value = stockMaterial.colour;
   updateForm.gramInStock.value = stockMaterial.gramInStock;
-  updateForm.minAmountReached.value = stockMaterial.minAmountReached;
+  updateForm.active.value = stockMaterial.active;
   updateForm.salesPrice.value = stockMaterial.salesPrice;
 }
 
@@ -23,7 +28,7 @@ function updateStockDataThroughForm(stockMaterial) {
   const material = form.material.value;
   const colour = form.colour.value;
   const gramInStock = Number(form.gramInStock.value);
-  const minAmountReached = form.minAmountReached.value;
+  const active = form.active.value;
   const salesPrice = Number(form.salesPrice.value);
 
   return {
@@ -32,7 +37,7 @@ function updateStockDataThroughForm(stockMaterial) {
     material,
     colour,
     gramInStock,
-    minAmountReached,
+    active,
     salesPrice
   };
 }
