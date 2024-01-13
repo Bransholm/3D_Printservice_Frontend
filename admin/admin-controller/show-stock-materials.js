8508a7e28483cebc94eb114f31e0f2cb88da82c0
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-13 22:37:27 +0100

    altered parameters

--- admin/admin-controller/show-stock-materials.js
+++ admin/admin-controller/show-stock-materials.js
@@ -1,37 +1,38 @@
 let stockClassList;
 import { createCatalogClasses } from "./instance-creator-admin.js";
 import { stockMaterial } from "../admin-view/admin-view-render-classes/stock-class.js";
-import { updateStockButtonClicked } from "./admin-main.js";
+import {updateStockButtonClicked} from "./admin-main.js"
 
 // showing all materials
 function showStockMaterials(stockMaterialData) {
   stockClassList = createCatalogClasses(stockMaterialData, stockMaterial);
-  renderStocks(stockClassList, "adminStockTableBody");
+
+  renderStocks(stockClassList);
 }
 
 // Create instances of the stock material class
-function renderStocks(listOfInstances, htmlId) {
+function renderStocks(listOfInstances) {
   console.log("No3. CallRenderMethod");
-  document.querySelector(`#${htmlId}`).innerHTML = "";
+  document.querySelector(`#adminStockTableBody`).innerHTML = "";
 
   for (const stockInstance of listOfInstances) {
     const stockHTML = stockInstance.render();
 
     document
-      .querySelector(`#${htmlId}`)
+      .querySelector(`#adminStockTableBody`)
       .insertAdjacentHTML("beforeend", stockHTML);
 
     //Fit the eventlistener first
-    eventListenerForStockUpdateButton(htmlId, stockInstance);
+    eventListenerForStockUpdateButton( stockInstance);
   }
 }
 
 // function triggered by the eventListener for stock update button.
-function eventListenerForStockUpdateButton(htmlId, classInstance) {
+function eventListenerForStockUpdateButton(classInstance) {
   // what eventlisteners to add for a given instance needs to go here...
 
   document
-    .querySelector(`#${htmlId} tr:last-child .btn_update_stock`)
+    .querySelector(`#adminStockTableBody tr:last-child .btn_update_stock`)
     .addEventListener("click", () => updateStockButtonClicked(classInstance));
 }
 
