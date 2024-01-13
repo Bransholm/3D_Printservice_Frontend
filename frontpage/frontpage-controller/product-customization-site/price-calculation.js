2a052a7cd90f75cef8d6a202da1fce14aac518ec
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-14 00:04:13 +0100

    total price and tax calculation module

--- frontpage/frontpage-controller/product-customization-site/price-calculation.js
+++ frontpage/frontpage-controller/product-customization-site/price-calculation.js
@@ -1,34 +1,18 @@
-function setCompleteProductPrice() {
-  const tax = 1.25;
-  const shipping = 39;
+import { shoppingCart } from "./shopping-cart.js";
 
-  document.querySelector("#productPrice").innerHTML = "";
-  // console.log(
-  //   `Samlet pris = materiale ${materialPrice}, størrelse${size}, antal${amount}`
-  // );
-  //der mangler en vloume udregning på baggrund af vægt i forhold til størrelsen.
-  setSingleProductPrice();
-  const bundlePrice = setBundleProductPrice();
+export function calculateTotalPrice() {
+  let accumulatedTax = 0;
+  let accumulatedPrice = 0;
 
-  price = bundlePrice * tax + shipping;
-  // run op!
-  document.querySelector(
-    "#productPrice"
-  ).innerHTML = `Samlet Pris: ${Math.round(price)} DKK`;
+  for (const item of shoppingCart) {
+    accumulatedPrice += Number(item.bundlePrice);
+    accumulatedTax += Number(item.bundleTax);
+  }
+
+  const totalOrderPrices = {
+    totalPrice: accumulatedPrice.toFixed(2),
+    totalTax: accumulatedTax.toFixed(2),
+  };
+
+  return totalOrderPrices;
 }
-
-function setSingleProductPrice() {
-  singleProductPrice = (materialPrice / 1000) * (size * 1.8);
-}
-
-function setBundleProductPrice() {
-  return productPrice * amount;
-}
-
-function calculateTotalOrderPrice(product){
-  const itemPrice = product.price - product.tax
-  
-}
-
-
-
