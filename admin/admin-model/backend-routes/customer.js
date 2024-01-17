eb0be1cbad4c59d48fc03da41b91940d20aaaf3e
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-17 17:53:48 +0100

    outcommented

--- admin/admin-model/backend-routes/customer.js
+++ admin/admin-model/backend-routes/customer.js
@@ -1,28 +1,28 @@
-async function catalogueCreateRoute(data) {
-  // console.log("PUTTING: ", data);
-  try {
-    const response = await fetch(`${endpoint}catalogue`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Add any additional headers if needed
-      },
-      body: JSON.stringify(data),
-    });
+// async function catalogueCreateRoute(data) {
+//   // console.log("PUTTING: ", data);
+//   try {
+//     const response = await fetch(`${endpoint}catalogue`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         // Add any additional headers if needed
+//       },
+//       body: JSON.stringify(data),
+//     });
 
-    console.log(response);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    } else {
-      const result = await response.json();
-      console.log("update successful! ", result);
-    }
+//     console.log(response);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     } else {
+//       const result = await response.json();
+//       console.log("update successful! ", result);
+//     }
 
-    return;
-  } catch (error) {
-    // Handle errors here
-    console.error("Error:", error);
-  }
-}
+//     return;
+//   } catch (error) {
+//     // Handle errors here
+//     console.error("Error:", error);
+//   }
+// }
 
-export { catalogueCreateRoute };
+// export { catalogueCreateRoute };
